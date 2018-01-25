1c392bfe122980486f8ae8976536dc72e4251cf4
Ulf Nordström <32666309+UffeSays@users.noreply.github.com>
2018-01-25 14:27:42 +0100

    Create Export_GEM.sql

---
+++ Export_GEM.sql
@@ -0,0 +1,7 @@
+
+-- GEMDTAP
+select * from GEMDTAP.GERMADO 
+select * from GEMDTAP.GEMAVOPF -- gemavopf
+select * from GEMDTAP.GEMLNDPF -- gemlndpf
+select * from GEMDTAP.GEMPNRPF -- gempnrpf
+select * from GEMDTAP.GEMSOCPF -- gemsocpf
