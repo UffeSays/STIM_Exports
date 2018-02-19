a1b0b76ff8f66632d68aea240316d50f60c9e3d5
Ulf Nordström <32666309+UffeSays@users.noreply.github.com>
2018-02-19 12:52:45 +0100

    Update export_NODDIS.sql

--- export_NODDIS.sql
+++ export_NODDIS.sql
@@ -1,5 +1,9 @@
 -- NORD-DIS (2) 
-select * from DINDTAS.DINUHSPF
+select * from DINDTAS.DINUHSPF -- not used
 select * from DINDTAS.DINMIPPF
+
+-- upphovspersoner
 select * from DISDTAP.FDISVE
+
+-- distinfo
 select * from DISDTAP.FDISVB
